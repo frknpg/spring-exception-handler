19134f1b95d3063f48bdabb58f7d8e552a74b141
Furkanp <frknpg@gmail.com>
2021-06-12 09:31:47 +0300

    Basic exception handle.
    Basic exception handle.
    
    Basic exception handle.

--- src/main/java/com/bafoly/ex/user/UserController.java
+++ src/main/java/com/bafoly/ex/user/UserController.java
@@ -1,5 +1,27 @@
 package com.bafoly.ex.user;
 
+import com.bafoly.ex.error.ApiError;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
-  
+
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUser(@PathVariable long id) {
+        try {
+            return ResponseEntity.ok(this.userService.getUserById(id));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ApiError(400, "User not found!", "/users/" + id));
+        }
+    }
+
 }
--- src/main/java/com/bafoly/ex/user/UserService.java
+++ src/main/java/com/bafoly/ex/user/UserService.java
@@ -1,5 +1,15 @@
 package com.bafoly.ex.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
-  
+
+    private final UserRepository userRepository;
+
+    public User getUserById(long id) {
+        return userRepository.findById(id).get();
+    }
 }
